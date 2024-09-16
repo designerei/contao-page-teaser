7475fa378597d0da0beee31a73f3f771abcc3044
Hendrik Bengs <hben@designerei-werbeagentur.de>
2024-09-16 08:02:56 +0200

    WIP

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -2,14 +2,23 @@
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
+$pm = PaletteManipulator::create()
+    ->addLegend('teaser_legend', 'meta_legend', PaletteManipulator::POSITION_AFTER, true)
+    ->addField('teaser', 'teaser_legend', PaletteManipulator::POSITION_APPEND)
+;
+
+foreach ($GLOBALS['TL_DCA']['tl_page']['palettes'] as $name => $palette) {
+    if ('__selector__' === $name) {
+        continue;
+    }
+
+    $pm->applyToPalette($name, 'tl_page');
+}
+
+unset($name, $palette, $pm);
+
 $GLOBALS['TL_DCA']['tl_page']['fields']['teaser'] = [
     'inputType' => 'textarea',
     'eval' => array('rte'=>'tinyMCE', 'tl_class'=>'clr'),
     'sql' => "text NULL",
 ];
-
-PaletteManipulator::create()
-    ->addLegend('teaser_legend', 'meta_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('teaser', 'teaser_legend', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('regular', 'tl_page')
-;
